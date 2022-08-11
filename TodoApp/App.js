1ba68ce39007006583cf64c97391f6bac8c034f1
Choi Young Jun <74344026+narvis2@users.noreply.github.com>
2022-08-11 14:38:06 +0900

    TODO Application Layout 추가
    □ TODO 가 비어있을 경우 보여줄 비어있는 Empty.js 추가
    □ AddTodo.js : TextInput 추가 및 click Listener 추가
    - AddTodo.js 파일에 TextInput 추가 및 클릭 했을 경우 해당 input 의 text를 빈값으로 만들어주고 키보드가 내려가도록 설정
    □ 앱의 상단(Toolbar)영역에 보여줄 Layout 추가
    □ 앱에 필요한 이미지를 담고있는 assets 디렉토리 추가

--- TodoApp/App.js
+++ TodoApp/App.js
@@ -5,107 +5,44 @@
  * @format
  * @flow strict-local
  */
-
+// KeyboardAvoidingView : 키보드가 화면을 가리지 않게 하기
 import React from 'react';
-import type {Node} from 'react';
 import {
   SafeAreaView,
-  ScrollView,
-  StatusBar,
   StyleSheet,
-  Text,
-  useColorScheme,
+  KeyboardAvoidingView,
+  Platform,
   View,
+  Text,
 } from 'react-native';
+import DateHead from './components/DateHead';
+import AddTodo from './components/AddTodo';
+import Empty from './components/Empty';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
+function App() {
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
-
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+    <SafeAreaView edges={['bottom']} style={styles.block}>
+      <KeyboardAvoidingView
+        behavior={Platform.select({
+          ios: 'padding',
+          android: undefined,
+        })}
+        style={styles.android}>
+        <DateHead />
+        <Empty />
+        <AddTodo />
+      </KeyboardAvoidingView>
     </SafeAreaView>
   );
-};
+}
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  block: {
+    flex: 1,
+    backgroundColor: 'white',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+  android: {
+    flex: 1,
   },
 });
 
