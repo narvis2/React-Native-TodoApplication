810b08956b7c698114eb52fde5d5f7f2bbd650fc
Choi Young Jun <74344026+narvis2@users.noreply.github.com>
2022-08-11 17:57:33 +0900

    TODO List 삭제 기능 추가
    ## TODO List 삭제 기능 추가
    - React-Native 에서 제공하는 Alert 을 사용하여 삭제 시 "정말 삭제 하시겠습니까?" 알럿을 띄우는 작업 추가
    - 삭제 후 List에서 해당 값을 제외한 나머지 값을 filter를 통해 가져와 다시 list에 넣어주는 코드 추가

--- TodoApp/components/TodoItem.js
+++ TodoApp/components/TodoItem.js
@@ -1,7 +1,46 @@
 import React from 'react';
-import {View, Text, StyleSheet, Image, TouchableOpacity} from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+  Alert,
+} from 'react-native';
+/**
+ * https://oblador.github.io/react-native-vector-icons/ 해당 사이트에서 확인 가능
+ * MaterialIcons 부분에는 해당 사이트의 빨간색 부분을 넣으면됨
+ * Icon name 부분에 아이콘 이름 넣으면됨.
+ */
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-function TodoItem({id, text, done, onToggle}) {
+function TodoItem({id, text, done, onToggle, onRemove}) {
+  const remove = () => {
+    // Android는 버튼에 스타일이 적용되지 않음. 필요하다면 Alert 처럼 보이는 컴포넌트를 직접 제작해야 함.
+    Alert.alert(
+      '삭제', // 제목
+      '정말로 삭제하시겠습니까?', // 내용
+      // 버튼 배열
+      [
+        // "취소" 버튼
+        {text: '취소', onPress: () => {}, style: 'cancel'},
+        // "삭제" 버튼
+        {
+          text: '삭제',
+          onPress: () => {
+            onRemove(id);
+          },
+          style: 'destructive',
+        },
+      ],
+      {
+        // Dialog 취소 가능 여부 ex) 다이얼로그 영역 외 클릭 했을 경우
+        cancelable: true,
+        // 취소 시 Callback 함수
+        onDismiss: () => {},
+      },
+    );
+  };
   return (
     <View style={styles.item}>
       <TouchableOpacity onPress={() => onToggle(id)}>
@@ -14,6 +53,13 @@ function TodoItem({id, text, done, onToggle}) {
         </View>
       </TouchableOpacity>
       <Text style={[styles.text, done && styles.lineThrough]}>{text}</Text>
+      {done ? (
+        <TouchableOpacity onPress={remove}>
+          <Icon name="delete" size={32} color="red" />
+        </TouchableOpacity>
+      ) : (
+        <View style={styles.removePlaceholder} />
+      )}
     </View>
   );
 }
@@ -48,6 +94,10 @@ const styles = StyleSheet.create({
   lineThrough: {
     color: '#929292',
     textDecorationLine: 'line-through',
+  },
+  removePlaceholder: {
+    width: 32,
+    height: 32,
   },
 });
 
